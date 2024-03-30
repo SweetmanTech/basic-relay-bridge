4c88d5fae4b7add24a5e18ccf4c7bcf8b1758565
Sweets Sweetman <sweetmantech@gmail.com>
2024-03-30 13:35:50 -0300

    I init <DestinationAmount /> component.

--- components/Swap/DestinationCard.tsx
+++ components/Swap/DestinationCard.tsx
@@ -1,25 +1,11 @@
-import { useBridgeProvider } from '@/providers/BridgeProvider';
-import Button from '../Button';
 import { Card } from '../Card/Card';
-import { formatEther } from 'viem';
-import ChainSelect from './ChainSelect';
-import Image from 'next/image';
-import getChainIcon from '@/lib/getChainIcon';
 import DestinationChain from './DestinationChain';
+import DestinationAmount from './DestinationAmount';
 
 const DestinationCard = () => {
-  const { bridgeAmount, destinationChain } = useBridgeProvider();
-
-  const handleSelectChange = async (selectedOption: string) => {
-    console.log('SWEETS CHANGES DESTINATION', selectedOption);
-  };
-
   return (
     <Card className="p-6 space-y-4">
-      <div className="flex justify-between items-center">
-        <span className="text-sm text-gray-500">To (estimated)</span>
-        <span className="text-2xl font-semibold">{formatEther(bridgeAmount)} ETH</span>
-      </div>
+      <DestinationAmount />
       <DestinationChain />
     </Card>
   );
