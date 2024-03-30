86d76c302892df732cde74cecf745c763e48481d
Sweets Sweetman <sweetmantech@gmail.com>
2024-03-30 12:37:46 -0300

    I fix basic edge cases: wallet not connected, pending tx, etc.

--- components/LandingPage/BridgeButton.tsx
+++ components/LandingPage/BridgeButton.tsx
@@ -3,19 +3,33 @@ import PendingTxModal from '../PendingTxModal';
 import useRelayBridge from '@/hooks/useRelayBridge';
 import usePrepareWallet from '@/hooks/usePrepareWallet';
 import { useBridgeProvider } from '@/providers/BridgeProvider';
+import { useState } from 'react';
+import Spinner from './Spinner';
 
 const BridgeButton = () => {
   const { prepare } = usePrepareWallet();
   const { bridge } = useRelayBridge();
   const { bridgeAmount, sourceTx } = useBridgeProvider() as any;
+  const [loading, setLoading] = useState<boolean>(false);
+
   const handleClick = async () => {
+    if (loading) return;
     if (!prepare()) return;
+    setLoading(true);
     await bridge((bridgeAmount as bigint).toString());
+    setLoading(false);
   };
 
   return (
     <div>
-      <Button onClick={handleClick}>Send</Button>
+      <Button
+        disabled={loading}
+        onClick={handleClick}
+        className="bg-[#ff5700] flex gap-5 min-h-[50px]"
+      >
+        {loading && <Spinner size={30} />}
+        {loading ? 'Bridging...' : 'Bridge'}
+      </Button>
       {sourceTx?.txHash && <PendingTxModal />}
     </div>
   );
