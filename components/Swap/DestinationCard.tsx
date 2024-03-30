dea3239b0d9854c94b7659ea1b79027d0c929eb9
Sweets Sweetman <sweetmantech@gmail.com>
2024-03-30 13:36:28 -0300

    I return component inline.

--- components/Swap/DestinationCard.tsx
+++ components/Swap/DestinationCard.tsx
@@ -2,13 +2,11 @@ import { Card } from '../Card/Card';
 import DestinationChain from './DestinationChain';
 import DestinationAmount from './DestinationAmount';
 
-const DestinationCard = () => {
-  return (
-    <Card className="p-6 space-y-4">
-      <DestinationAmount />
-      <DestinationChain />
-    </Card>
-  );
-};
+const DestinationCard = () => (
+  <Card className="p-6 space-y-4">
+    <DestinationAmount />
+    <DestinationChain />
+  </Card>
+);
 
 export default DestinationCard;
